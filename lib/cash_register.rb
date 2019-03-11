6acff09fe7ed166274403e14002b27a893d9d584
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-11 00:48:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   def add_item(title,price,quantity=1)
     @total += price*quantity
     quantity.times { @items << title }
-    @last_item = self
+    @last_item_total = price*quantity
   end
   def apply_discount
     if @discount == 0
