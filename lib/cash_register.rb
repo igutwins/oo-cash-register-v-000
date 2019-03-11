7737327666690d034a35646d91a33a2116fa1f55
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-11 00:33:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,11 @@ class CashRegister
     @total += price*quantity
   end
   def apply_discount
+    if @discount = 0
+      "There is no discount to apply."
+    else
     @total = @total - @total*@discount/100
     "After the discount, the total comes to $#{@total}."
+  end
   end
 end
